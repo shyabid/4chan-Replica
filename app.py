594cb87a8d65459fb81e958b74c5942913c28471
Abid <136696763+shyabid@users.noreply.github.com>
2023-12-31 22:53:13 +0600

    deploy fix 1

--- app.py
+++ app.py
@@ -31,4 +31,8 @@ def handle_connect():
     socketio.emit('initial_messages', messages, room=request.sid)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=5000)
+
